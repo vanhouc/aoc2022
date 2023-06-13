e00910cfce4db0c9af2ca2af582952aa7e17c318
Cameron VanHouzen <camvh@live.com>
2023-06-13 14:05:55 -0400

    complete part 1 solution

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let items: Vec<char> = ('a'..='z').chain('A'..='Z').collect();
+    let input = include_str!("input.txt");
+
+    let sum: usize = input
+        .lines()
+        .filter_map(|line| {
+            let (first, second) = line.split_at(line.len() / 2);
+            let first: HashSet<char> = first.chars().collect();
+            let second: HashSet<char> = second.chars().collect();
+            Some(
+                first
+                    .intersection(&second)
+                    .find_map(|c| items.iter().position(|i| i == c))?
+                    + 1,
+            )
+        })
+        .sum();
+
+    println!("{:?}", sum)
 }
